8ef6041f88898ea16553d4eb925d7c1076c3c2fa
James Juett <jjuett@umich.edu>
2017-01-31 10:45:17 -0500

    bugfix for server_mode in config

--- public/api.php
+++ public/api.php
@@ -17,7 +17,7 @@ function dbConnect() {
 }
 
 $app = new \Slim\Slim(array(
-    'mode' => $GLOBALS["server_mode"]
+    'mode' => $GLOBALS["config"]["server_mode"]
 ));
 
 // Only invoked if mode is "production"
