e6db15899c87ce998a773f3c42b757ca498ea773
alexbro <alexbro@umich.edu>
2017-02-20 17:31:03 -0500

    Detecting breaking the interface for Matrix and Image

--- public/js/core/analysis.js
+++ public/js/core/analysis.js
@@ -2,57 +2,57 @@ var Lobster = Lobster || {};
 
 Lobster.Analysis = {};
 
-var Bird = Lobster.Analysis.Bird = Class.extend({
-    _name: "Bird",
-
-    init : function(name, age) {
-        // Create and initialize instance variable
-        this.i_name = name;
-        this.i_age = age;
-        //this.egg = // create an egg object.
-    },
-
-    getName : function() {
-        return this.i_name;
-    }
-
-});
-
-
-var Chicken = Lobster.Analysis.Chicken = Bird.extend({
-    _name: "Chicken"
-
-    // inherit bird constructor implicitly
-});
-
-var Duck = Lobster.Analysis.Duck = Bird.extend({
-    _name: "Duck",
-
-    // new ctor for Duck. Takes in name, age is optional (default 0)
-    // also initializes new member variable for Duck beyond Bird stuff
-    init : function(name, age) {
-        age = age || 0;
-        this.initParent(name, age || 0);
-        this.i_numDucklings = 0;
-    },
-
-    setAge : function(newAge) {
-        this.name = newAge;
-        delete this.name;
-    },
-
-    // example overriding
-    getName : function() {
-        // all ducks are named daffy
-        return "daffy";
-    }
-
-    // x || y
-    // x ? x : y
-    //
-    // x && y
-    // x ? y : false
-});
+// var Bird = Lobster.Analysis.Bird = Class.extend({
+//     _name: "Bird",
+//
+//     init : function(name, age) {
+//         // Create and initialize instance variable
+//         this.i_name = name;
+//         this.i_age = age;
+//         //this.egg = // create an egg object.
+//     },
+//
+//     getName : function() {
+//         return this.i_name;
+//     }
+//
+// });
+//
+//
+// var Chicken = Lobster.Analysis.Chicken = Bird.extend({
+//     _name: "Chicken"
+//
+//     // inherit bird constructor implicitly
+// });
+//
+// var Duck = Lobster.Analysis.Duck = Bird.extend({
+//     _name: "Duck",
+//
+//     // new ctor for Duck. Takes in name, age is optional (default 0)
+//     // also initializes new member variable for Duck beyond Bird stuff
+//     init : function(name, age) {
+//         age = age || 0;
+//         this.initParent(name, age || 0);
+//         this.i_numDucklings = 0;
+//     },
+//
+//     setAge : function(newAge) {
+//         this.name = newAge;
+//         delete this.name;
+//     },
+//
+//     // example overriding
+//     getName : function() {
+//         // all ducks are named daffy
+//         return "daffy";
+//     }
+//
+//     // x || y
+//     // x ? x : y
+//     //
+//     // x && y
+//     // x ? y : false
+// });
 
 // check for IntegralConversion coming from LValueToRValue coming from an Assignment
 // if (isA(top.if, Conversions.IntegralConversion)){
@@ -63,8 +63,23 @@ var Duck = Lobster.Analysis.Duck = Bird.extend({
 //     }
 // }
 
+
+
 var analyze = Lobster.analyze = function(program, codeEditor) {
 
+    // Type Matrix
+    var matrixType = program.globalScope.lookup("Matrix").type;
+
+    // Pointer to type Matrix
+    var matrixPointerType = Types.Pointer.instance(matrixType);
+
+    // Type Image
+    var imageType = program.globalScope.lookup("Image").type;
+
+    // Pointer to type Image
+    var imagePointerType = Types.Pointer.instance(imageType);
+
+    // BFS of the children of code constructs
     var bfsChildren = function(queue, func) {
 
         while (queue.length > 0) {
@@ -80,26 +95,42 @@ var analyze = Lobster.analyze = function(program, codeEditor) {
 
 
     bfsChildren(program.topLevelDeclarations, function(construct){
-        if (isDot(construct)) {
-            console.log("Here's a dot! " + construct.toString() + " line " + construct.code.line);
-            if (!construct.context.implicit) { // If it's not implicitly defined (i.e. not in original source code), don't annotate it
-                codeEditor.addAnnotation(GutterAnnotation.instance(construct, "dot", "boop"));
+        if (isDotOrArrow(construct)) {
+
+            // If it's not implicitly defined (i.e. not in original source code), don't annotate it
+            if (!construct.context.implicit) {
+
+                // TODO MAKE A FUNCTION
+
+                // Check if breaking Matrix interface with a Matrix or Matrix*
+                if (similarType(construct.operand.type, matrixType) ||
+                    similarType(construct.operand.type, matrixPointerType)) {
+
+                    // If this operand is being used outside of a function starting with Matrix_, that's bad
+                    if (!construct.context.func.name.startsWith("Matrix_")) {
+                        codeEditor.addAnnotation(GutterAnnotation.instance(construct, "breakInterface", "Breaking the interface!"));
+                    }
+                }
+
+                // Check if breaking Image interface with Image or Image*
+                if (similarType(construct.operand.type, imageType) ||
+                    similarType(construct.operand.type, imagePointerType)) {
+
+                    // If this operand is being used outside of a function starting with Matrix_, that's bad
+                    if (!construct.context.func.name.startsWith("Image_")) {
+                        codeEditor.addAnnotation(GutterAnnotation.instance(construct, "breakInterface", "Breaking the interface!"));
+                    }
+                }
             }
         }
     });
-  // var myBird = Bird.instance("Myrtle II", 3);
-  // console.log(myBird.getName());
-  //
-  // var myDuck = Duck.instance("scrooge");
-
-  // NEVER DO THIS from outside "private" scope
-//    obj.member = blah
 
 };
 
-// Determines if a construct is a dot operator
-var isDot = function(construct) {
-  return isA(construct, Expressions.Dot);
+
+// Function for detecting dots or arrow operators to detect breaking the interface
+var isDotOrArrow = function(construct) {
+    return isA(construct, Expressions.Dot) || isA(construct, Expressions.Arrow);
 };
 
 // var other = {
