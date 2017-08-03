9c849576fe4b9eabaf6b325e7e01252aa9f522c5
James Juett <jjuett@umich.edu>
2017-08-03 01:10:09 -0400

    Added a regression test for simple declarations. The test runs the code and confirms no assertions failed. Added a new verifier to support checking for failed assertions, which actually runs a Simulation based on the given program. Added functions to Simulation to report assertion failures and crashes. Also fixed a bug caused by the fix for #25, which was still consuming too many keyboard events. Should realllly be fixed now :).

--- public/js/core/regression.js
+++ public/js/core/regression.js
@@ -4,10 +4,10 @@ var TestVerifier = Class.extend({
     SUCCESS : {status: "success", message: "test successful"},
     verify : function() {
         try{
-            return this.i_verifyImpl.apply(this, arguments);
+            return mixin({testName: this.classString()}, this.i_verifyImpl.apply(this, arguments));
         }
         catch(e) {
-            return {status: "exception", message: "The test crashed with an uncaught exception", exception: e};
+            return {testName: this.classString(), status: "exception", message: "The test crashed with an uncaught exception", exception: e};
         }
     }
 });
@@ -26,6 +26,21 @@ var NoErrorsNoWarningsVerifier = TestVerifier.extend({
     }
 });
 
+var NoAssertionFailuresVerifier = TestVerifier.extend({
+    _name: "NoAssertionFailureVerifier",
+    i_verifyImpl : function(program) {
+        var sim = Simulation.instance(program);
+        sim.start();
+        sim.runToEnd();
+        if (!sim.hasAssertionFailureOccurred()) {
+            return TestVerifier.SUCCESS;
+        }
+        else {
+            return {status: "failure", message: "An assertion in the program failed when run."};
+        }
+    }
+});
+
 var ProgramTest = Class.extend({
     _name: "ProgramTest",
 
@@ -33,14 +48,18 @@ var ProgramTest = Class.extend({
         this.i_defaultReporter = reporter;
     },
 
-    init : function(sourceFiles, translationUnits, verifier, reporter) {
+    init : function(sourceFiles, translationUnits, verifiers, reporter) {
+        if (!Array.isArray(verifiers)) { verifiers = [verifiers]; }
         reporter = reporter || this.i_defaultReporter;
         this.program = Program.instance();
         sourceFiles.forEach(this.program.addSourceFile.bind(this.program));
         translationUnits.forEach(this.program.createTranslationUnitForSourceFile.bind(this.program));
         this.program.fullCompile();
 
-        this.results = verifier.verify(this.program);
+        var self = this;
+        this.results = verifiers.map(function(verifier) {
+            return verifier.verify(self.program);
+        });
 
         reporter && reporter(this);
     }
@@ -49,7 +68,7 @@ var ProgramTest = Class.extend({
 var SingleTranslationUnitTest = ProgramTest.extend({
     _name: "SingleTranslationUnitTest",
 
-    init : function(sourceText, verify) {
-        this.initParent([SourceFile.instance("test.cpp", sourceText)], ["test.cpp"], verify);
+    init : function(sourceText, verifiers) {
+        this.initParent([SourceFile.instance("test.cpp", sourceText)], ["test.cpp"], verifiers);
     }
 });
