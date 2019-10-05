1f60c3d691c6fa494946a4ded7f25f5b05b911b6
James Juett <jjuett@umich.edu>
2019-10-05 00:19:48 -0400

    fix errors in objects.ts

--- src/js/core/types.ts
+++ src/js/core/types.ts
@@ -1032,6 +1032,9 @@ export class ClassType extends ObjectType {
     public readonly precedence: number = 0;
     public readonly isArithmeticType: boolean = false;
     public readonly isComplete: boolean = false;
+
+    public readonly name: string = "";
+
     public sameType(other: Type): boolean {
         throw new Error("Method not implemented.");
     }
