563fad95cc1869f11bd8753a85a6b07eee62e3fd
Christina <cmfh@umich.edu>
2020-05-17 16:40:37 -0400

    Fixed code view panel and started starter code

--- dash/dash/js/overview/CodeViewer.tsx
+++ dash/dash/js/overview/CodeViewer.tsx
@@ -5,165 +5,175 @@ import CodeCard from "./CodeCard";
 
 export default function CodeViewer() {
   return (
-    <fieldset className="border p-2 mh-100">
+    <fieldset className="border rounded p-2 ">
       <legend className="w-auto">Code</legend>
-      <Tabs defaultActiveKey="home" transition={false} id="noanim-tab-example">
-        <Tab eventKey="home" title="Student Solutions">
-          <Container className="d-none d-lg-block">
-            <Row className="py-2">
-              <Col>
-                <CodeCard />
-              </Col>
-              <Col>
-                <CodeCard />
-              </Col>
-              <Col>
-                <CodeCard />
-              </Col>
-            </Row>
-            <Row className="py-2">
-              <Col>
-                <CodeCard />
-              </Col>
-              <Col>
-                <CodeCard />
-              </Col>
-              <Col>
-                <CodeCard />
-              </Col>
-            </Row>
-            <Row className="py-2">
-              <Col>
-                <CodeCard />
-              </Col>
-              <Col>
-                <CodeCard />
-              </Col>
-              <Col>
-                <CodeCard />
-              </Col>
-            </Row>
-            <Row className="py-2">
-              <Col>
-                <CodeCard />
-              </Col>
-              <Col>
-                <CodeCard />
-              </Col>
-              <Col>
-                <CodeCard />
-              </Col>
-            </Row>
-            <Row className="py-2">
-              <Col>
-                <CodeCard />
-              </Col>
-              <Col>
-                <CodeCard />
-              </Col>
-              <Col>
-                <CodeCard />
-              </Col>
-            </Row>
-            <Row className="py-2">
-              <Col>
-                <CodeCard />
-              </Col>
-              <Col>
-                <CodeCard />
-              </Col>
-              <Col>
-                <CodeCard />
-              </Col>
-            </Row>
-          </Container>
-          <Container className="d-none d-sm-block d-lg-none">
-            <Row className="py-2">
-              <Col>
-                <CodeCard />
-              </Col>
-              <Col>
-                <CodeCard />
-              </Col>
-            </Row>
-            <Row className="py-2">
-              <Col>
-                <CodeCard />
-              </Col>
-              <Col>
-                <CodeCard />
-              </Col>
-            </Row>
-            <Row className="py-2">
-              <Col>
-                <CodeCard />
-              </Col>
-              <Col>
-                <CodeCard />
-              </Col>
-            </Row>
-            <Row className="py-2">
-              <Col>
-                <CodeCard />
-              </Col>
-              <Col>
-                <CodeCard />
-              </Col>
-            </Row>
-            <Row className="py-2">
-              <Col>
-                <CodeCard />
-              </Col>
-              <Col>
-                <CodeCard />
-              </Col>
-            </Row>
-            <Row className="py-2">
-              <Col>
-                <CodeCard />
-              </Col>
-              <Col>
-                <CodeCard />
-              </Col>
-            </Row>
-          </Container>
-          <Container className="d-xs-block d-sm-none">
-            <Row className="py-2">
-              <Col>
-                <CodeCard />
-              </Col>
-            </Row>
-            <Row className="py-2">
-              <Col>
-                <CodeCard />
-              </Col>
-            </Row>
-            <Row className="py-2">
-              <Col>
-                <CodeCard />
-              </Col>
-            </Row>
-            <Row className="py-2">
-              <Col>
-                <CodeCard />
-              </Col>
-            </Row>
-            <Row className="py-2">
-              <Col>
-                <CodeCard />
-              </Col>
-            </Row>
-            <Row className="py-2">
-              <Col>
-                <CodeCard />
-              </Col>
-            </Row>
-          </Container>
-        </Tab>
-        <Tab eventKey="profile" title="Starter Code">
-          <StarterCode />
-        </Tab>
-      </Tabs>
+      <div className="code-view d-flex flex-column">
+        <Tabs
+          defaultActiveKey="solutions"
+          transition={false}
+          id="all-code-view-tabs"
+        >
+          <Tab eventKey="solutions" title="Student Solutions">
+            <div className="flex-grow-1 overflow-auto">
+              <Container className="d-none d-lg-block">
+                <Row className="py-2">
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                </Row>
+                <Row className="py-2">
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                </Row>
+                <Row className="py-2">
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                </Row>
+                <Row className="py-2">
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                </Row>
+                <Row className="py-2">
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                </Row>
+                <Row className="py-2">
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                </Row>
+              </Container>
+              <Container className="d-none d-sm-block d-lg-none">
+                <Row className="py-2">
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                </Row>
+                <Row className="py-2">
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                </Row>
+                <Row className="py-2">
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                </Row>
+                <Row className="py-2">
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                </Row>
+                <Row className="py-2">
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                </Row>
+                <Row className="py-2">
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                </Row>
+              </Container>
+              <Container className="d-xs-block d-sm-none">
+                <Row className="py-2">
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                </Row>
+                <Row className="py-2">
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                </Row>
+                <Row className="py-2">
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                </Row>
+                <Row className="py-2">
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                </Row>
+                <Row className="py-2">
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                </Row>
+                <Row className="py-2">
+                  <Col>
+                    <CodeCard />
+                  </Col>
+                </Row>
+              </Container>
+            </div>
+          </Tab>
+          <Tab eventKey="starter-code" title="Starter Code">
+            <div className="py-2 flex-row flex-grow-1">
+              <StarterCode />
+            </div>
+          </Tab>
+        </Tabs>
+      </div>
     </fieldset>
   );
 }
--- dash/dash/js/overview/StarterCode.tsx
+++ dash/dash/js/overview/StarterCode.tsx
@@ -1,8 +1,34 @@
 import React from "react";
-import { Tabs, Tab } from "react-bootstrap";
+import { Tab, Row, Col, Nav } from "react-bootstrap";
 
 export default function StarterCode() {
   return (
-    <h1>I am starter code</h1>
+    <Tab.Container
+      id="individual-code-view-tabs"
+      defaultActiveKey="first"
+    >
+      <Row>
+        <Col sm={3}>
+          <Nav variant="pills" className="flex-column">
+            <Nav.Item>
+              <Nav.Link eventKey="first">Really long file 1</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="second">File 2</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Col>
+        <Col sm={9}>
+          <Tab.Content>
+            <Tab.Pane eventKey="first">
+              <p>This is starter file 1.</p>
+            </Tab.Pane>
+            <Tab.Pane eventKey="second">
+              <p>This is starter file 2.</p>
+            </Tab.Pane>
+          </Tab.Content>
+        </Col>
+      </Row>
+    </Tab.Container>
   );
 }
