66005cb341abc724c4a670576cd160abd59a4268
Christina <cmfh@umich.edu>
2020-05-28 22:47:43 -0400

    Add a go to lobster button

--- dash/dash/js/overview/right-panel/FileTabs.tsx
+++ dash/dash/js/overview/right-panel/FileTabs.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Tab, Row, Col, Nav } from "react-bootstrap";
+import { Tab, Row, Col, Nav, Button } from "react-bootstrap";
 import FileView from "./FileView";
 
 interface Props {
@@ -12,28 +12,50 @@ interface State {}
 class FileTabs extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
+
+    this.goToLobster = this.goToLobster.bind(this);
+  }
+
+  goToLobster() {
+    alert("This is where I would send you to lobster");
   }
 
   render() {
     const { fileList, baseUrl } = this.props;
 
+    const tabs = (
+      <Nav variant="pills" className="flex-column overflow-auto">
+        <div className="mb-3 border-bottom">
+          <Button
+            variant="outline-primary"
+            className="mb-3 w-100"
+            onClick={this.goToLobster}
+          >
+            Open in Lobster
+          </Button>
+        </div>
+        {fileList.map((filename, idx) => (
+          <Nav.Item key={idx}>
+            <Nav.Link eventKey={idx}>{filename}</Nav.Link>
+          </Nav.Item>
+        ))}
+      </Nav>
+    );
+    const tabContent = (
+      <Tab.Content>
+        {fileList.map((filename, idx) => (
+          <FileView key={idx} eventKey={idx} fileUrl={baseUrl + filename} />
+        ))}
+      </Tab.Content>
+    );
+
     const fullSizeLayout = (
       <Row className="h-100 d-none d-sm-flex">
         <Col sm={3} className="d-flex flex-column h-100">
-          <Nav variant="pills" className="flex-column overflow-auto">
-            {fileList.map((filename, idx) => (
-              <Nav.Item key={idx}>
-                <Nav.Link eventKey={idx}>{filename}</Nav.Link>
-              </Nav.Item>
-            ))}
-          </Nav>
+          {tabs}
         </Col>
         <Col sm={9} className="d-flex flex-column h-100">
-          <Tab.Content>
-            {fileList.map((filename, idx) => (
-              <FileView key={idx} eventKey={idx} fileUrl={baseUrl + filename} />
-            ))}
-          </Tab.Content>
+          {tabContent}
         </Col>
       </Row>
     );
@@ -41,28 +63,20 @@ class FileTabs extends React.Component<Props, State> {
     const smallLayout = (
       <Row className="h-100 d-xs-flex d-sm-none">
         <Col sm={3} className="d-flex flex-column h-25">
-          <Nav variant="pills" className="flex-column overflow-auto">
-            {fileList.map((filename, idx) => (
-              <Nav.Item key={idx}>
-                <Nav.Link eventKey={idx}>{filename}</Nav.Link>
-              </Nav.Item>
-            ))}
-          </Nav>
+          {tabs}
         </Col>
         <Col sm={9} className="d-flex flex-column h-75">
-          <Tab.Content>
-            {fileList.map((filename, idx) => (
-              <FileView key={idx} eventKey={idx} fileUrl={baseUrl + filename} />
-            ))}
-          </Tab.Content>
+          {tabContent}
         </Col>
       </Row>
     );
 
-
-
     return (
-      <Tab.Container id="individual-code-view-tabs" transition={false} defaultActiveKey={0}>
+      <Tab.Container
+        id="individual-code-view-tabs"
+        transition={false}
+        defaultActiveKey={0}
+      >
         {fullSizeLayout}
         {smallLayout}
       </Tab.Container>
