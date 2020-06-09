d8656b897e1e5dfa970f74a13f29203786acde7d
James Juett <jjuett@umich.edu>
2020-06-09 15:00:18 -0400

    random cleanup

--- src/js/core/expressions.ts
+++ src/js/core/expressions.ts
@@ -2323,138 +2323,6 @@ export class RuntimeAddressOfExpression<T extends PointerType> extends SimpleRun
 
 
 
-// export var UnaryOp  = Expression.extend({
-//     _name: "UnaryOp",
-//     i_childrenToExecute : ["operand"],
-//     i_childrenToExecuteForMemberOverload : ["operand", "funcCall"], // does not include rhs because function call does that
-//     i_childrenToExecuteForOverload : ["funcCall"], // does not include rhs because function call does that
-
-//     i_createFromAST : function(ast, context){
-//         UnaryOp._parent.i_createFromAST.apply(this, arguments);
-//         this.operator = ast.operator;
-//     },
-
-//     compile : function(){
-
-//         var auxOperand = CPPConstruct.create(this.ast.operand, {parent: this, auxiliary: true});
-//         auxOperand.compile();
-
-//         if (isA(auxOperand.type, Types.Class)){
-//             // If it's of class type, we look for overloads
-//             var overloadOp = auxOperand.type.classScope.singleLookup("operator" + this.operator, {
-//                 own:true, paramTypes:[], isThisConst : auxOperand.type.isConst
-//             });
-//             if (!overloadOp) {
-//                 overloadOp = this.contextualScope.singleLookup("operator" + this.operator, {
-//                     paramTypes: [auxOperand.type]
-//                 });
-//             }
-
-//             if (overloadOp){
-//                 this.isOverload = true;
-//                 this.isMemberOverload = isA(overloadOp, MemberFunctionEntity);
-
-//                 if (this.isMemberOverload){
-//                     // Member overload means operand is our direct child, and no arguments to function call
-//                     this.operand = this.i_createAndCompileChildExpr(this.ast.operand);
-//                     this.funcCall = FunctionCall.instance({args: []}, {parent:this});
-//                     this.funcCall.compile({func: overloadOp});
-//                     this.i_childrenToExecute = this.i_childrenToExecuteForMemberOverload;
-//                 }
-//                 else{
-//                     // Non-member overload means operand is the argument to the function call
-//                     this.funcCall = FunctionCall.instance({args: [this.ast.operand]}, {parent:this});
-//                     this.funcCall.compile({func: overloadOp});
-//                     this.i_childrenToExecute = this.i_childrenToExecuteForOverload;
-//                 }
-//                 this.type = this.funcCall.type;
-//                 this.valueCategory = this.funcCall.valueCategory;
-//             }
-//             else{
-
-//                 // TODO: this appears to allow compilation to proceed for a class-type operand with
-//                 // no overloads found, but that's doomed to fail (I think?). Perhaps my thought was
-//                 // the error messages provided if you accidentally used a unary operator e.g. * with
-//                 // a class-type operand were more illustrative if they said something like "you can't use
-//                 // * with a non-pointer type rather than oops i can't find an overload for * with this class type
-//                 this.operand = this.i_createAndCompileChildExpr(this.ast.operand);
-//                 this.convert();
-//                 this.typeCheck();
-//                 this.compileTemporarires();
-//             }
-//         }
-//         else{
-//             this.operand = this.i_createAndCompileChildExpr(this.ast.operand);
-//             this.convert();
-//             this.typeCheck();
-//             this.compileTemporarires();
-//         }
-//     },
-
-//     upNext : function(sim: Simulation, rtConstruct: RuntimeConstruct){
-//         // Push lhs, rhs, and function call (if lhs class type)
-//         var toReturn = Expression.upNext.apply(this, arguments);
-
-//         // If using an assignment operator, set receiver for function call instance
-//         if (this.funcCall && this.isMemberOverload){
-//             inst.childInstances.funcCall.calledFunction.setReceiver(EvaluationResultRuntimeEntity.instance(this.operand.type, inst.childInstances.operand));
-//         }
-
-//         return toReturn;
-//     },
-
-//     stepForward: function(sim: Simulation, rtConstruct: RuntimeConstruct){
-//         if (inst.index === "operate"){
-//             if (this.funcCall){
-//                 // Assignment operator function call has already taken care of the "assignment".
-//                 // Just evaluate to returned value from assignment operator.
-//                 inst.setEvalResult(inst.childInstances.funcCall.evalResult);
-//                 this.done(sim, inst);
-//                 //return true;
-//             }
-//             else{
-//                 this.operate(sim, inst);
-//                 this.done(sim, inst);
-//             }
-//         }
-//     },
-
-//     operate: Class._ABSTRACT,
-
-//     isTailChild : function(child){
-//         return {isTail: false,
-//             reason: "The " + this.operator + " operation will happen after the recursive call.",
-//             others: [this]
-//         };
-//     }
-// });
-
-// export var Dereference = UnaryOp.extend({
-//     _name: "Dereference",
-//     valueCategory: "lvalue",
-//     },
-
-// });
-
-// export var AddressOf = UnaryOp.extend({
-//     _name: "AddressOf",
-//     valueCategory: "prvalue",
-
-//     typeCheck : function(){
-//         // operand must be an lvalue
-//         if(this.operand.valueCategory !== "lvalue"){
-//             this.addNote(CPPError.expr.addressOf.lvalue_required(this));
-//         }
-
-//         this.type = Types.Pointer.instance(this.operand.type);
-//     },
-
-//     operate: function(sim: Simulation, rtConstruct: RuntimeConstruct){
-//         var obj = inst.childInstances.operand.evalResult;
-
-//         inst.setEvalResult(obj.getPointerTo());
-//     }
-// });
 
 
 // export var UnaryPlus = UnaryOp.extend({
@@ -2598,9 +2466,6 @@ export class RuntimeAddressOfExpression<T extends PointerType> extends SimpleRun
 //     }
 // });
 
-
-// // TODO: Consolidate postfix increment/decrement into one class.  consider also merging subscript
-// // TODO: Allow overriding postfix increment/decrement
 // export var Increment  = Expression.extend({
 //     _name: "Increment",
 //     valueCategory: "prvalue",
@@ -2977,28 +2842,6 @@ export class RuntimeSubscriptExpression<T extends ObjectType = ObjectType> exten
 
 
 
-
-// export var PREDEFINED_FUNCTIONS = {
-//     rand : function(args, sim, inst){
-//         return Value.instance(Math.floor(sim.nextRandom() * 32767), Types.Int.instance());
-//     },
-//     "assert" : function(args, sim, inst){
-//         if(!args[0].evalResult.value){
-//             sim.assertionFailure("Yikes! An assert failed! <br /><span class='code'>" + inst.model.getSourceText() + "</span> on line " + inst.model.getSourceText() + ".");
-//         }
-//         return Value.instance("", Types.Void.instance());
-//     },
-//     "pause" : function(args, sim, inst){
-//         sim.pause();
-//         return Value.instance("", Types.Void.instance());
-//     },
-//     "pauseIf" : function(args, sim, inst){
-//         if(args[0].evalResult.value){
-//             sim.pause();
-//         }
-//         return Value.instance("", Types.Void.instance());
-//     }
-// };
 
 export type PostfixExpressionASTNode =
     StaticCastExpressionASTNode |
@@ -4165,6 +4008,7 @@ interface MagicFunctionImpl {
     readonly operate: <RT extends PotentialReturnType>(rt: RuntimeMagicFunctionCallExpression<RT>) => void;
 }
 
+// TODO: add some RNG function?
 const MAGIC_FUNCTIONS : {[k in MAGIC_FUNCTION_NAMES]: MagicFunctionImpl} = {
     assert: {
         returnType: VoidType.VOID,
