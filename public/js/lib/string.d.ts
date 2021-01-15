c472f48e671d00e993dda4956087f7c9557492f5
James Juett <jjuett@umich.edu>
2021-01-15 00:29:55 -0500

    redo public folder

--- public/js/lib/string.d.ts
+++
@@ -1,3 +0,0 @@
-import { MemberSubobject, CPPObject } from "../core/objects";
-import { Char, PointerType, CompleteClassType } from "../core/types";
-export declare function getDataPtr(obj: CPPObject<CompleteClassType>): MemberSubobject<PointerType<Char>>;
