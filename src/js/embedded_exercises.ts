471e379e5fbc50a656ad18f23c6683272253102d
James Juett <jjuett@umich.edu>
2021-02-18 07:20:24 -0500

    some random stuff and readme

--- src/js/embedded_exercises.ts
+++ src/js/embedded_exercises.ts
@@ -17,21 +17,22 @@ $(() => {
         $(this).append(createRunestoneExerciseOutlet(""+exID));
 
         let filename = $(this).find(".lobster-ex-file-name").html()?.trim() ?? "file.cpp";
-        let projectName = $(this).find(".lobster-ex-project-name").html()?.trim() ?? "UnnamedProject";
+        let exKey = $(this).find(".lobster-ex-key").html()?.trim() ??
+                          $(this).find(".lobster-ex-project-name").html()?.trim() ?? "UnnamedProject";
 
-        let exerciseSpec = getExerciseSpecification(projectName) ?? DEFAULT_EXERCISE;
+        let exerciseSpec = getExerciseSpecification(exKey) ?? DEFAULT_EXERCISE;
 
-        let completionMessage = $(this).find(".lobster-ex-complete-message").html()?.trim();
+        let completionMessage = $(this).find(".lobster-ex-completion-message").html()?.trim() ?? $(this).find(".lobster-ex-complete-message").html()?.trim();
         if (completionMessage) {
           exerciseSpec.completionMessage = completionMessage;
         }
-        let initCode = decode($(this).find(".lobster-ex-init-code").html()?.trim() ?? "");
+        let initCode = decode($(this).find(".lobster-ex-starter-code").html()?.trim() ?? $(this).find(".lobster-ex-init-code").html()?.trim() ?? "");
         if (initCode) {
           exerciseSpec.starterCode = initCode;
         }
 
         let project = new Project(
-          projectName,
+          exKey,
           undefined,
           [{name: filename, code: exerciseSpec.starterCode, isTranslationUnit: true}],
           new Exercise(exerciseSpec));
