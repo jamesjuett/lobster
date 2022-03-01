11fd6e0c2fe54ad1b3d1d912cbb65f20864e985a
James Juett <jjuett@umich.edu>
2022-03-01 13:21:33 -0500

    remove old unused files

--- old/provision.sh
+++
@@ -1,6 +0,0 @@
-    echo "CREATE USER 'lobster'@'localhost' IDENTIFIED BY 'devpass'" | mysql -uroot -proot
-    echo "CREATE DATABASE lobster" | mysql -uroot -proot
-    echo "GRANT ALL ON lobster.* TO 'lobster'@'localhost'" | mysql -uroot -proot
-
-    mysql -uroot -proot lobster < /var/www/lobster_init.sql
-
