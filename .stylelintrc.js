9f4105bb20f1bd5b421f6087f1fd66eaa0a58de1
xcesiv <10799441+xcesiv@users.noreply.github.com>
2022-03-02 03:20:27 -0500

    👌 IMPROVE: Add more robust stylelint functionality
    Signed-off-by: xcesiv <10799441+xcesiv@users.noreply.github.com>

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,21 @@
-{
-  "extends": [
-    "stylelint-prettier/recommended"
-  ]
-}
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-css-modules',
+    'stylelint-config-prettier',
+  ],
+  plugins: ['stylelint-declaration-block-no-ignored-properties'],
+  rules: {
+    'no-descending-specificity': null,
+    //https://github.com/stylelint/stylelint/issues/4114
+    'function-url-quotes': 'always',
+    'selector-attribute-quotes': 'always',
+    'font-family-no-missing-generic-family-keyword': null,
+    'plugin/declaration-block-no-ignored-properties': true,
+    'unit-no-unknown': [true, { ignoreUnits: ['rpx'] }],
+    // webcomponent
+    'selector-type-no-unknown': null,
+    'value-keyword-case': ['lower', { ignoreProperties: ['composes'] }],
+  },
+  ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.tsx', '**/*.ts'],
+};
